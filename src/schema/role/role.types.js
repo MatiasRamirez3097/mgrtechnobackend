9a1b98ddd1682872052024b12324964cc295c421
Matias Ramirez <matiasgr3009@gmail.com>
2021-11-09 11:39:14 -0300

    graphql setting up

---
+++ src/schema/role/role.types.js
@@ -0,0 +1,20 @@
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+    extend type Query {
+        roles: [Role]
+        userRoles: [UserRole]
+        # userRoles(userId: ID!): [UserRole]
+    }
+    type Role {
+        id: ID!
+        name: String!
+    }
+    
+    type UserRole {
+        userId: ID!
+        roleId: ID!
+    }
+`
+
+module.exports = typeDefs
